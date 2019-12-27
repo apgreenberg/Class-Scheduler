e859504b80ecea38d1fbb58f75fe261b65e4b0c1
apgreenberg <43008006+apgreenberg@users.noreply.github.com>
2019-12-27 16:14:16 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include "School.cpp"
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::begin;
+using std::end;
+using std::getline;
+int main() {
+  School *s = new School();
+  string name;
+  cout << "Hi new student!" << endl;
+  cout << "Please enter your full name" << endl;
+  getline(cin,name);
+  vector<string> fullName;
+  string str;
+  for (char x:name) {
+    if (x == ' ') {
+        fullName.push_back(str);
+        str.erase();
+      } else {
+        str=str+x;
+      }
+  }
+  fullName.push_back(str);
+  string firstName = fullName[0];
+  string middleName = "";
+  string lastName = "";
+  if (fullName.size() == 2) {
+    lastName = fullName[1];
+  } else {
+    middleName = fullName[1];
+    lastName = fullName[fullName.size()-1];
+  }
+  s->addStudent(firstName, middleName, lastName);
+  s->addTeacher("Lauren", "", "O'Malley", vector<int>{100});
+  s->addRoom(230, 1, 32);
+  s->addClass(100, "AP Research");
+  s->printSchedule(1);
+  return 0;
+}
